a328db431da397cca12eb5696bd2eb47abd7ab92
Rene Olsthoorn <rene.olsthoorn@gmail.com>
2022-10-28 21:09:36 +0200

    -

--- js/flipperen.js
+++ js/flipperen.js
@@ -11,14 +11,17 @@ reneo.Flipperen.Main = (function () {
 			balls = [],
 			engine,
 			render;
+
 			
         function init() {
 			container = $(theContainer);
         }
+
 		
 		function matterEngine() {
 			return engine;
 		}
+
 
 		function update() {
 			Matter.Engine.update(engine, 1000 / 60);
@@ -32,25 +35,15 @@ reneo.Flipperen.Main = (function () {
 			}
 		}
 		
-		function insertNewDynamicBall(i) {
-			let ballImage = createBallImage();
-			container.append(ballImage);
-			
-			let ballBody = Matter.Bodies.circle(20 + (i * 34), 16, Ball.radius, { isStatic: false, restitution: 1.0, friction: 0.0005 });
-			
-			Matter.Composite.add(engine.world, [ ballBody ]);
-			
-			let newBall = new Ball(reneo.Flipperen.singletonMain, ballImage, ballBody);
-			if (i <= balls.length) {
-				balls[i] = newBall;
-			} else {
-				balls.push(newBall);
-			}
-		}		
 		
 		function go() {
 			engine = Matter.Engine.create();
 			engine.enableSleeping = true;
+			
+			var render = Matter.Render.create({
+				element: document.body,
+				engine: engine
+			});
 			
 			var ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 			var wallLeft = Matter.Bodies.rectangle(0, 0, 60, 1200, { isStatic: true });
@@ -77,8 +70,11 @@ reneo.Flipperen.Main = (function () {
 
 			collisionEndHandler();
 			
+			Matter.Render.run(render);
+			
 			window.requestAnimationFrame(step);
 		}
+
 		
 		function collisionEndHandler() {
 			Matter.Events.on(engine, 'collisionEnd', function(event) {
@@ -94,6 +90,7 @@ reneo.Flipperen.Main = (function () {
 				}
 			});
 		}
+
 		
 		function step(timestamp) {
 			if (startTs === undefined) {
@@ -103,16 +100,36 @@ reneo.Flipperen.Main = (function () {
 			update();
 			window.requestAnimationFrame(step);
 		}
+
+
+		function insertNewDynamicBall(i) {
+			let ballImage = createBallImage();
+			container.append(ballImage);
+			
+			let ballBody = Matter.Bodies.circle(20 + (i * 34), 16, Ball.radius, { isStatic: false, restitution: 1.0, friction: 0.0005 });
+			
+			Matter.Composite.add(engine.world, [ ballBody ]);
+			
+			let newBall = new Ball(reneo.Flipperen.singletonMain, ballImage, ballBody);
+			if (i <= balls.length) {
+				balls[i] = newBall;
+			} else {
+				balls.push(newBall);
+			}
+		}
+		
 		
 		function createBallImage() {
 			let newBall = $('<img class="flipperbal" src="/assets/voetbal32x32.png" alt="ball" width="32" height="32" style="display: none">');
 			return newBall;
 		}
 		
+		
 		function createFlipperInDOM() {
 			flipper = $('<svg height="60" width="160"><polygon points="0,30 160,60 160,0" style="fill:blue;stroke:purple;stroke-width:1" /></svg>');
 			container.append(flipper);
 		}
+
 
 		this.go = go;
 		this.matterEngine = matterEngine;
