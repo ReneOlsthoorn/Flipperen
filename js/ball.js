0a25899c1fb6373293c7338b6d12a86caef9d870
Olsthoorn, René (Q.A.) <rene.olsthoorn@politie.nl>
2022-10-28 05:30:11 +0200

    matterjs rulez!

--- js/ball.js
+++ js/ball.js
@@ -2,32 +2,17 @@
 class Ball {
 	ballImage;
 	ballBody;
-	ballCollisionShape;
 	
 	static radius = 16;
-	static mass = 3;
 	
-	constructor(theBallImage, theBallBody, theBallCollisionShape) {
+	constructor(theBallImage, theBallBody) {
 		this.ballImage = theBallImage;
 		this.ballBody = theBallBody;		
-		this.ballCollisionShape = theBallCollisionShape;
 	}
 	
 	updateDomPosition() {
-		let newPos = this.ballBody.getPos();
-		this.ballImage.css({ 'transform' : `translate(${newPos.x}px, ${640-newPos.y}px) rotate(${this.getAngle()}rad)` });
+		let newPos = this.ballBody.position;
+		let newAngle = this.ballBody.angle;
+		this.ballImage.css({ 'transform' : `translate(${newPos.x}px, ${newPos.y}px) rotate(${newAngle}rad)` });
 	}
-	
-	setPos(pos) {
-		this.ballBody.setPos(pos);
-	}
-	
-	getAngle() {
-		let vel = this.ballBody.rot;
-		return 2*Math.PI - cp.v.toangle(vel);
-	}
-	
-	radiansToDegrees(radians) {
-		return radians * (180 / Math.PI);
-	}	
 }
--- js/flipperen.js
+++ js/flipperen.js
@@ -7,135 +7,45 @@ reneo.Flipperen.Main = (function () {
     
     function Main(theContainer) {
 		var container,
-			balls = [],
 			startTs,
-			raf,
+			balls = [],
 			engine,
 			render;
 			
         function init() {
 			container = $(theContainer);
-			raf = window.requestAnimationFrame
-				|| window.webkitRequestAnimationFrame
-				|| window.mozRequestAnimationFrame
-				|| window.oRequestAnimationFrame
-				|| window.msRequestAnimationFrame
-				|| function(callback) {
-					return window.setTimeout(callback, 1000 / 60);
-				};
-			engine = Matter.Engine.create();
-			render = Matter.Render.create({
-				element: document.body,
-				engine: engine
-			});
-			
-			// create two boxes and a ground
-			var boxA = Matter.Bodies.circle(400, 200, 32, {
-    isStatic:false,
-    restitution: 0.95
-  });
-			var boxB = Matter.Bodies.rectangle(500, 50, 80, 80);
-			var ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });	
-
-			// add all of the bodies to the world
-			Matter.Composite.add(engine.world, [boxA, boxB, ground]);
         }
-		
-		function addFloor() {
-		}	
 
-		function update(dt) {
+		function update() {
 			Matter.Engine.update(engine, 1000 / 60);
-			//space.step(dt);
-			//for (let ball of balls) {
-			//	ball.updateDomPosition();
-			//}
+			
+			//var bodies = Matter.Composite.allBodies(engine.world);
+			for (let ball of balls) {
+				ball.updateDomPosition();
+			}
 		}
 		
 		function go() {
-			// run the renderer
-			Matter.Render.run(render);
-
-			// create runner
-			//var runner = Matter.Runner.create();
-
-			// run the engine
-			//Matter.Runner.run(runner, engine);
+			engine = Matter.Engine.create();
 			
-			window.requestAnimationFrame(step);
-
-/*
-			space = new cp.Space();
-			space.iterations = 60;
-			space.gravity = v(0, -500);
-			space.sleepTimeThreshold = 0.5;
-			space.collisionSlop = 0.5;
+			var ground = Matter.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+			var wallLeft = Matter.Bodies.rectangle(0, 0, 60, 1200, { isStatic: true });
+			var wallRight = Matter.Bodies.rectangle(800, 0, 60, 1200, { isStatic: true });
+			Matter.Composite.add(engine.world, [ ground, wallLeft, wallRight ]);			
 			
-			addFloor();
-			
-			staticBallImage = createBallImage();
-			container.append(staticBallImage);
-			
-			let staticBallShape = space.addShape(new cp.CircleShape(space.staticBody, Ball.radius, v(64, 64)));
-			staticBallShape.setElasticity(1);
-			staticBallShape.setFriction(1);
-			staticBallShape.setLayers(NOT_GRABABLE_MASK);
-			
-			staticBall = new StaticBall(staticBallImage, staticBallShape);
-			staticBall.updateDomPosition();
-
-			for (let i = 1; i < 10; i++ ) {
+			for (let i = 1; i < 10; i++) {
 				let ballImage = createBallImage();
 				container.append(ballImage);
 				
-				let ballBody = space.addBody(new cp.Body(Ball.mass, cp.momentForCircle(Ball.mass, 0, Ball.radius, v(0, 0))));
+				let ballBody = Matter.Bodies.circle(100 + (i * 30), 0 + (i * 40), Ball.radius, { isStatic: false, restitution: 1.0 });
 				
-				let ballCollisionShape = space.addShape(new cp.CircleShape(ballBody, Ball.radius, v(0, 0)));
-				ballCollisionShape.setElasticity(1);
-				ballCollisionShape.setFriction(1);
+				Matter.Composite.add(engine.world, [ ballBody ]);
 				
-				let newBall = new Ball(ballImage, ballBody, ballCollisionShape);
+				let newBall = new Ball(ballImage, ballBody);
 				balls.push(newBall);
-				
-				newBall.setPos(v(200 + i*30, 300+ (2 * (Ball.radius*1) + 5) * i));
 			}
 			
-			// add flipper
-			flipper = $('<svg height="60" width="160"><polygon points="0,30 160,60 160,0" style="fill:blue;stroke:purple;stroke-width:1" /></svg>');
-			
-			let tris = [
-                cp.v(0, 30),
-                cp.v(160, 60),
-                cp.v(160, 0)
-            ];
-			
-			let polyMass = 100;
-			let momentPoly = cp.momentForPoly(polyMass, tris, v(0,0));
-			momentPoly = 1385000;
-			console.log("Moment poly: " + momentPoly);
-			flipperBody = space.addBody(new cp.Body(polyMass, momentPoly));
-			flipperBody.setPos(v(100, 100));
-			
-			let flipperShape = space.addShape(new cp.PolyShape(flipperBody, tris, v(0,0))); //space.staticBody     v(95, 150)
-			//let flipperShape = space.addShape(new cp.PolyShape(space.staticBody, tris, v(95, 150))); //space.staticBody     v(95, 150)
-			//flipperShape.setElasticity(0.4);
-			//flipperShape.setFriction(1);
-			//flipperBody.setPos(v(100, 200));
-			
-			flipperJointBody = space.addBody(new cp.Body(Infinity, Infinity));
-			flipperJointBody.setPos(v(100, 100));
-			flipperPinJoint = new cp.PinJoint(flipperJointBody, flipperBody, cp.v(160,30), cp.v(160,30));
-			space.addConstraint(flipperPinJoint);
-			space.addConstraint(new cp.DampedRotarySpring(flipperBody, flipperJointBody, 0.15, 20000000, 900000));
-
-			container.append(flipper);	
-							
 			window.requestAnimationFrame(step);
-			
-			setTimeout(function() {
-				flipperJointBody.applyImpulse(40000, v(0, 0));
-			}, 3000);
-			*/
 		}
 		
 		function step(timestamp) {
@@ -143,13 +53,18 @@ reneo.Flipperen.Main = (function () {
 				startTs = timestamp;
 			}
 			const elapsed = timestamp - startTs;
-			update(1/60);
+			update();
 			window.requestAnimationFrame(step);
 		}
 		
 		function createBallImage() {
-			//let newBall = $('<img class="flipperbal" src="/assets/voetbal32x32.png" alt="ball" width="32" height="32">');
-			//return newBall;
+			let newBall = $('<img class="flipperbal" src="/assets/voetbal32x32.png" alt="ball" width="32" height="32">');
+			return newBall;
+		}
+		
+		function createFlipperInDOM() {
+			flipper = $('<svg height="60" width="160"><polygon points="0,30 160,60 160,0" style="fill:blue;stroke:purple;stroke-width:1" /></svg>');
+			container.append(flipper);
 		}
 
 		this.go = go;
